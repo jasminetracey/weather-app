eadc154a6bd39dc50a038555793d4e1ef7c0aad3
Jasmine Tracey <traceyjas@gmail.com>
2022-02-17 16:58:16 -0500

    setup next application

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import "../styles/globals.css";
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>Weather App</title>
+        <meta name="description" content="Weather App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default MyApp;
